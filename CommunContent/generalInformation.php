68faf9db478b1dc549a8f4770e79bc17133f07a0
jtissier78 <jtissier78@gmail.com>
2019-11-28 15:24:08 +0100

    first commit

--- CommunContent/generalInformation.php
+++ CommunContent/generalInformation.php
@@ -1,47 +1,34 @@
 <img src="" alt="<?php echo $lang['GenInfo']['alt']['Photo'];?>" >
 <ul id="GenInfo-bloc">
     <li class="BlocInfoGen-Content">
-        <div class="BlocInfoGen-Content-Logo">
-            <img src="img/IconId.png" alt="<?php echo $lang['GenInfo']['alt']['Logo-Name'];?>" class="Logo">
-        </div>
         <div class="BlocInfoGen-Content-Texte"><?php echo $lang['GenInfo']['Name']; ?> : <span class="InfoGenerale">Julien Tissier</span></div>
     </li>
     <li class="BlocInfoGen-Content Localisation" >
-        <div class="BlocInfoGen-Content-Logo">
-            <img src="" alt="<?php echo $lang['GenInfo']['alt']['Logo-Local'];?>" class="Logo">
-        </div>
+        <!-- <img src="" alt="<?php /* echo $lang['GenInfo']['alt']['Logo-Local']; */?>" class="Logo BlocInfoGen-Content-Logo"> -->
         <div class="BlocInfoGen-Content-Texte"><?php echo $lang['GenInfo']['Local']; ?> : <span class="InfoGenerale">Bourges (18) <span class="italique">France</span></span></div>
     </li>
     <li class="BlocInfoGen-Content">
-        <div class="BlocInfoGen-Content-Logo">
-            <img src="" alt="<?php echo $lang['GenInfo']['alt']['Logo-BirthDay'];?>" class="Logo">
-        </div>
+        <!-- <img src="" alt="<?php /* echo $lang['GenInfo']['alt']['Logo-BirthDay']; */?>" class="Logo BlocInfoGen-Content-Logo"> -->
         <div class="BlocInfoGen-Content-Texte"><?php echo $lang['GenInfo']['BirthDay']; ?> : <span class="InfoGenerale"><?php echo $lang['GenInfo']['BirthDay-Date']; ?></span></div>
     </li>
     <li class="BlocInfoGen-Content"><a href="mailto:jtissier78@gmail.com">
-        <div class="BlocInfoGen-Content-Logo">
-            <img src="" alt="<?php echo $lang['GenInfo']['alt']['Logo-mail'];?>" class="Logo">
-        </div>
+        <!-- <img src="" alt="<?php /* echo $lang['GenInfo']['alt']['Logo-mail']; */?>" class="Logo BlocInfoGen-Content-Logo"> -->
         <div class="BlocInfoGen-Content-Texte"><span class="InfoGenerale">jtissier78@gmail.com</span></div>
     </a></li>
     <li class="BlocInfoGen-Content">
-        <div class="BlocInfoGen-Content-Logo">
-            <img src="" alt="<?php echo $lang['GenInfo']['alt']['Logo-phone'];?>" class="Logo">
-        </div>
+        <!-- <img src="" alt="<?php /* echo $lang['GenInfo']['alt']['Logo-phone']; */?>" class="Logo BlocInfoGen-Content-Logo"> -->
         <div class="BlocInfoGen-Content-Texte"><span class="InfoGenerale">06.58.33.82.14</span></div>
     </li>
     <li class="BlocInfoGen-Content"><a href="http://">
-        <div class="BlocInfoGen-Content-Logo">
-            <img src="" alt="<?php echo $lang['GenInfo']['alt']['Logo-web'];?>" class="Logo">
-        </div>
+        <!-- <img src="" alt="<?php echo $lang['GenInfo']['alt']['Logo-web'];?>" class="Logo BlocInfoGen-Content-Logo"> -->
         <div class="BlocInfoGen-Content-Texte"><span class="InfoGenerale">Mon Site web</span></div>
     </a></li>
     <li class="BlocInfoGen-Content"><ul>
-        <li><a href="" class="BlocOnfoGen-Content-Logo"><img src="" alt="Logo Facebook" class="Logo"></a></li>
-        <li><a href="" class="BlocOnfoGen-Content-Logo"><img src="" alt="Logo LinkedIn" class="Logo"></a></li>
+        <!-- <li><a href="" class="BlocOnfoGen-Content-Logo"><img src="" alt="Logo Facebook" class="Logo"></a></li> -->
+        <li><a href="" class="BlocOnfoGen-Content-Logo"><img src="img/linkedin.svg" alt="Logo LinkedIn" class="Logo"></a></li>
         <li><a href="" class="BlocOnfoGen-Content-Logo"><img src="img/GitHub-Mark-32px.png" alt="Logo GitHub" class="Logo"></a></li>
-        <li><a href="" class="BlocOnfoGen-Content-Logo"><img src="" alt="Logo StackOverFlow" class="Logo"></a></li>
-        <li><a href="" class="BlocOnfoGen-Content-Logo"><img src="" alt="Logo OpenClassroom" class="Logo"></a></li>
-        <li><a href="" class="BlocOnfoGen-Content-Logo"><img src="" alt="Logo Codeur.com" class="Logo"></a></li>
+        <!-- <li><a href="" class="BlocOnfoGen-Content-Logo"><img src="" alt="Logo StackOverFlow" class="Logo"></a></li> -->
+        <!-- <li><a href="" class="BlocOnfoGen-Content-Logo"><img src="" alt="Logo OpenClassroom" class="Logo"></a></li> -->
+        <!-- <li><a href="" class="BlocOnfoGen-Content-Logo"><img src="" alt="Logo Codeur.com" class="Logo"></a></li> -->
     </ul></li>
 </ul>
